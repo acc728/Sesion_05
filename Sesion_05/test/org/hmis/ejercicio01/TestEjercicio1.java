f0b0fd14285baf176f3df8b07d96e5141d0a1580
Adrian Camacho Capilla <acc728@inlumine.ual.es>
2022-05-09 11:46:36 +0200

    Prueba de git webhook

--- Sesion_05/test/org/hmis/ejercicio01/TestEjercicio1.java
+++ Sesion_05/test/org/hmis/ejercicio01/TestEjercicio1.java
@@ -8,15 +8,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class TestEjercicio1 {
 
-//	@Test
-//	void test1() {
-//		// Arrange
-//		Ejercicio01 e1 = new Ejercicio01();
-//		// Act
-//		int resultado = e1.transformar(210); //Es multiplo de 2, 3 y 5 por lo que cubre los tres casos de una vez
-//		// Assert
-//		assertEquals(7,resultado); //expected, actual
-
 	@ParameterizedTest(name = "{index} => transformar {0} sale {1}")
 	@CsvSource({ "4, 1", "9, 1", "35, 7" })
 	void testEjercicio01(int numero, int resto) {
